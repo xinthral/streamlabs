bd844b4833192381d15824eb1cbce0ea3f53e371
Jesse <xinthral@yahoo.com>
2021-04-10 22:07:51 -0400

    Foundations for SLCB plugin of xbot features

---
+++ xbot/phrase_StreamlabsSystem.py
@@ -0,0 +1,61 @@
+"""
+SLCB runs on IronPython, those cheeky whores:
+https://stackoverflow.com/a/66794423/13825434
+"""
+try:
+    import sqlite3
+except:
+    import clr
+    clr.AddReference("IronPython.SQLite.dll")
+    clr.AddReference("IronPython.Modules.dll")
+finally:
+    import sys
+    import nt
+    sys.path.insert(1, nt.getcwd() + "\\Services\\Scripts\\xbot")
+    from xsql import Database
+    import random
+    import sqlite3
+    import time
+
+ScriptName = "Phrase"
+Website = "https://www.lmgtfy.com"
+Description = "Phrase Script for the feelz"
+Creator = "Xinthral"
+Version = "0.0.1"
+Command = "!phrase"
+SQLTable = 'phrases'
+TextRepo = Database.queryTableAll(SQLTable)
+random.seed(time.time())
+
+def getText():
+    global TextRepo
+    if len(TextRepo) <= 1:
+        lastValue = TextRepo.pop(0)
+        TextRepo = Database.queryTableAll(SQLTable)
+        return(lastValue)
+    return(TextRepo.pop(random.randrange(0, len(TextRepo))))
+
+def send_message(message):
+    Parent.SendStreamMessage(message)
+    return
+
+def Init():
+    """ Constructor API Method """
+    return
+
+def Execute(data):
+    """ Command Execution Method """
+    if data.GetParam(0) != Command:
+        return
+
+    username = data.UserName
+    for line in getText()[1].split(Database._delim):
+        send_message(line)
+        time.sleep(2)
+    return
+
+def Tick():
+    """ Timed Event Loop Method """
+    return
+
+# Xinthral's Sql Handler file
---
+++ xbot/xsql.py
@@ -0,0 +1,123 @@
+"""
+SLCB runs on IronPython, those cheeky whores:
+https://stackoverflow.com/a/66794423/13825434
+"""
+try:
+    import sqlite3
+except:
+    import clr
+    clr.AddReference("IronPython.SQLite.dll")
+    clr.AddReference("IronPython.Modules.dll")
+finally:
+    import re
+    import sqlite3
+
+class Database:
+    """ Static Class Scope Variables """
+    _delim = ';::;'
+    _library = 'Services/Scripts/xbot/library.db'
+    _tables = ['jokes', 'phrases', 'wur']
+
+    @staticmethod
+    def create_connection(db_file=_library):
+        """ Establish Connection to database object and return connection object """
+        conn = None
+        try:
+            conn = sqlite3.connect(db_file)
+        except Exception as e:
+            raise(e)
+        return(conn)
+
+    @staticmethod
+    def queryTableAll(tbl_name=_tables[0]):
+        """ Query all items from a specific database in the database object """
+        con = Database.create_connection()
+        c = con.cursor()
+        c.execute("SELECT * FROM {}".format(tbl_name))
+        rows = c.fetchall()
+        con.close()
+        return(rows)
+
+    @staticmethod
+    def getTableHeaders(tbl_name=_tables[0]):
+        """ Query Headers for specific table in database object """
+        return([ele[1] for ele in Database.showTableSchema(tbl_name)])
+
+    @staticmethod
+    def insert(payload, tbl_name=_tables[0], delim=_delim):
+        if len(payload[0].split(delim)) < 1:
+            print('Empty Payload')
+            return(False)
+
+        con = Database.create_connection()
+        c = con.cursor()
+        columns = Database.getTableHeaders(tbl_name)
+        columns = ', '.join(columns)
+        elements = [str(Database.getTableCount(tbl_name))] + list(payload)
+        # Yeah, this next line is gross
+        elements = ', '.join([("\"{}\"".format(str(ele).encode('UTF-8').replace('"', r"\'"))) for ele in elements])
+        sqlQuery = "INSERT INTO {} ({}) VALUES ({})".format(tbl_name, columns, elements)
+        c.execute(sqlQuery)
+        con.commit()
+        con.close()
+        return(True)
+
+    @staticmethod
+    def insertJoke(payload):
+        """ Inserts joke payload into database object (wrapper) """
+        #payload: ['input text', 'category', blocked: 0/1]
+        return(Database.insert(payload, 'jokes'))
+
+    @staticmethod
+    def insertPhrase(payload):
+        """ Inserts phrase payload into database object (wrapper) """
+        #payload: ['input text', 'category', blocked: 0/1]
+        return(Database.insert(payload, 'phrases'))
+
+    @staticmethod
+    def insertRather(payload):
+        """ Inserts would you rather payload into database object (wrapper) """
+        #payload: ['input text', 'category', blocked: 0/1]
+        return(Database.insert(payload, 'wur'))
+
+    @staticmethod
+    def showTables():
+        """ Query table names from database object """
+        con = Database.create_connection()
+        c = con.cursor()
+        c.execute("SELECT name FROM sqlite_master WHERE type=\'table\'")
+        rows = c.fetchall()
+        con.close()
+        return(rows)
+
+    @staticmethod
+    def showTableSchema(tbl_name=_tables[0]):
+        """ Query Schema for specific table in database object """
+        con = Database.create_connection()
+        c = con.cursor()
+        c.execute("PRAGMA table_info({})".format(tbl_name))
+        rows = c.fetchall()
+        con.close()
+        return(rows)
+
+    @staticmethod
+    def getTableCount(tbl_name=_tables[0]):
+        """ Query item count from a specific table in database object """
+        con = Database.create_connection()
+        c = con.cursor()
+        c.execute("SELECT COUNT(*) FROM {}".format(tbl_name))
+        count = c.fetchone()[0]
+        con.close()
+        return(count)
+
+    @staticmethod
+    def queryTableCategory(tbl_name=_tables[0], cat='dad'):
+        """
+        Query all elements for a specific category from a table in the database object
+        """
+        con = Database.create_connection()
+        c = con.cursor()
+        c.execute("SELECT * FROM {} WHERE category=\'{}\'".format(tbl_name, cat))
+        rows = c.fetchall()
+        con.close()
+        return(rows)
