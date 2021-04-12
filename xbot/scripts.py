1351af0258103381e2676942b83e69cc68d072e7
Jesse <xinthral@yahoo.com>
2021-04-12 07:02:58 -0400

    Corrected conditional logic

--- xbot/scripts.py
+++ xbot/scripts.py
@@ -41,13 +41,14 @@ def getText(Parent, SQLTable):
     if SQLTable == 'wur':
         TextRepo = Rathers
 
-    if len(TextRepo) <= 1:
-        if SQLTable == 'facts':
-            responseText = (0, getFact(Parent),)
-        else:
-            responseText = TextRepo.pop(0)
-            TextRepo = Database.queryTableAll(SQLTable)
-        
+    if SQLTable == 'facts':
+        responseText = (0, getFact(Parent),)
+    elif len(TextRepo) == 0:
+        TextRepo = Database.queryTableAll(SQLTable)
+    elif len(TextRepo) == 1:
+        responseText = TextRepo.pop(0)
+        TextRepo = Database.queryTableAll(SQLTable)
+
     if responseText == None:
         responseText = TextRepo.pop(random.randrange(0, len(TextRepo)))
 
