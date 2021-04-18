bbf32c7939095a248d31895bffe45174a7e331ab
Jesse <xinthral@yahoo.com>
2021-04-18 16:04:39 -0400

    Displays and menu's now showing up as intended

--- xbot/dbtables.py
+++ xbot/dbtables.py
@@ -2,46 +2,53 @@ from xsql import Database
 """
 Handy Suite Console Scripts to interact with the database
 """
-def displayMenu():
+def menu():
     menu = [[1, 'Display an item in more readable formats'],
     [2, 'Enable / Disable an item'],
     [3, 'Change category of item']]
-    print("User Menu:\n")
+    print("User Menu:")
     for _ in menu:
-        print("\t{:2} :: {}".format(_[0], _[1]))
+        print(" {:2} :: {}".format(_[0], _[1]))
 
 def displayTable(query):
     print 'Database output begins...'
     print("{:4} || {:3} || {:16} || {}".format('ID', 'Off', 'Category', 'Content'))
     for element in Database.queryTableAll(query):
-        print("{:4} || {:3} || {:16} || {}".format(element[0], 1 if element[3] == 0 else 0, element[2], element[1]))
+        print("{:4} || {:3} || {:16} || {}".format(element[0], element[3], element[2], element[1]))
     print 'Database output complete...'
+    # paused = raw_input('Press Enter to continue...')
 
-def inspectTable(user_input):
-        user_input = raw_input("{}\nWhich table would you like to inspect?:".format(Database._tables))
-        if user_input == 'f':
-            displayTable('facts')
-        elif user_input == 'j':
-            displayTable('jokes')
-        elif user_input == 'p':
-            displayTable('phrases')
-        elif user_input == 'r':
-            displayTable('rather')
-        else:
-            pass
+def inspectTable(table):
+    displayTable(table)
 
-def main():
     repeat = True
     while (repeat):
-        displayMenu()
+        menu()
         choice = raw_input('Choose an option: (q to quit): ')
-        user_input = choice[0].lower()
+        user_input = choice.strip()
         if user_input == '1':
-            inpsectTable()
+            pass
         elif user_input == '2':
             pass
         elif user_input == '3':
             pass
+        else:
+            repeat = False
+
+def main():
+    repeat = True
+    while (repeat):
+        table = None
+        user_input = raw_input("{}\nWhich table would you like to inspect? (q to quit): ".format(Database._tables))
+        choice = user_input[0].lower()
+        if choice == 'f':
+            inspectTable('facts')
+        elif choice == 'j':
+            inspectTable('jokes')
+        elif choice == 'p':
+            inspectTable('phrases')
+        elif choice == 'r':
+            inspectTable('rather')
         else:
             repeat = False
 
